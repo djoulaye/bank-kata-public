5208178ee09ee8d7b238b448119fc7759b561920
djoulaye <50450593+djoulaye@users.noreply.github.com>
2020-06-06 21:58:41 +0200

    Init

---
+++ src/test/java/fr/ing/interview/AppTest.java
@@ -0,0 +1,18 @@
+package fr.ing.interview;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
