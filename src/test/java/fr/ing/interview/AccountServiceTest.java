d0311302acc5dd48513c41314a83ff45c9a16900
djoulaye <50450593+djoulaye@users.noreply.github.com>
2020-06-26 23:11:29 +0200

    Add 2 tests

---
+++ src/test/java/fr/ing/interview/AccountServiceTest.java
@@ -0,0 +1,19 @@
+package fr.ing.interview;
+
+
+import org.junit.jupiter.api.Test;
+
+
+class AccountServiceTest {
+
+    @Test
+    void given_superior_amount_of_0_01_when_deposit_on_existing_account_then_accepted_deposit() {
+
+    }
+
+    @Test
+    void given_inferior_amount_of_0_01_when_deposit_on_existing_account_then_refused_deposit() {
+
+    }
+
+}
