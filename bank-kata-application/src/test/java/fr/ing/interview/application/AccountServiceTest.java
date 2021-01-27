9db156547dae1b8be037f2001827e3182be412df
djoulaye <50450593+djoulaye@users.noreply.github.com>
2021-01-28 00:19:10 +0100

    Add test on deposit method in AccountServiceTest

--- bank-kata-application/src/test/java/fr/ing/interview/application/AccountServiceTest.java
+++ bank-kata-application/src/test/java/fr/ing/interview/application/AccountServiceTest.java
@@ -2,16 +2,19 @@ package fr.ing.interview.application;
 
 import fr.ing.interview.application.exception.AlreadyExistsAccountException;
 import fr.ing.interview.application.exception.UnknownAccountException;
+import fr.ing.interview.domain.Account;
 import fr.ing.interview.domain.AccountRepository;
 import fr.ing.interview.domain.Operation;
+import fr.ing.interview.domain.OperationDirection;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,6 +22,7 @@ class AccountServiceTest {
 
     private static final String EXISTING_ACCOUNT_NUMBER = "123";
     private static final String NOT_EXISTING_ACCOUNT_NUMBER = "124";
+    public static final double AMOUNT_10_00 = 10.00;
 
     @Mock
     private AccountRepository accountRepository;
@@ -30,6 +34,17 @@ class AccountServiceTest {
     @Test
     void deposit_amount_on_not_existing_account_should_throw_exception() {
         assertThatThrownBy(() -> accountService.depositMoney(NOT_EXISTING_ACCOUNT_NUMBER, any(Operation.class))).isInstanceOf(UnknownAccountException.class);
+    }
+
+    @Test
+    void deposit_amount_on_existing_account_should_not_throw_exception() {
+        //Arrange
+        Account account = new Account("EXISTING_ACCOUNT_NUMBER");
+        when(accountRepository.findByAccountNumber(EXISTING_ACCOUNT_NUMBER)).thenReturn(Optional.of(account));
+        Operation depositOperation = new Operation(OperationDirection.CREDIT, AMOUNT_10_00);
+
+        //Act & Assert
+        assertThatCode(() -> accountService.depositMoney(EXISTING_ACCOUNT_NUMBER, depositOperation)).doesNotThrowAnyException();
     }
 
     @Test
