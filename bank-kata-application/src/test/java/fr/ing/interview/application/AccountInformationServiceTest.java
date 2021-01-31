e722b913d57dbc204c7602514af088f870b923d6
mnscrtx <msncrtx@gmail.com>
2021-01-31 22:13:05 +0100

    Account information service unit test with 2 levels of mock

--- bank-kata-application/src/test/java/fr/ing/interview/application/AccountInformationServiceTest.java
+++ bank-kata-application/src/test/java/fr/ing/interview/application/AccountInformationServiceTest.java
@@ -1,11 +1,15 @@
 package fr.ing.interview.application;
 
 import fr.ing.interview.domain.Account;
+import fr.ing.interview.domain.AccountRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.mockito.Mockito.when;
@@ -13,14 +17,17 @@ import static org.mockito.Mockito.when;
 @ExtendWith(MockitoExtension.class)
 class AccountInformationServiceTest {
 
-//    @Mock
-//    private AccountRepository accountRepository;
+    @Mock
+    private AccountRepository accountRepository;
 
     @InjectMocks
     private AccountInformationService accountInformationService;
 
-    @Mock
-    private AccountService accountService;
+    //    @Spy
+//    @InjectMocks
+//    private AccountService accountService = new AccountService();
+    @InjectMocks
+    private AccountService accountService = Mockito.spy(new AccountService());
 
     private static final String EXISTING_ACCOUNT_NUMBER = "123";
 
@@ -30,8 +37,8 @@ class AccountInformationServiceTest {
         //Arrange
         Account account = new Account(EXISTING_ACCOUNT_NUMBER);
 //        AccountService accountService = new AccountService();
-//        when(accountRepository.findByAccountNumber(EXISTING_ACCOUNT_NUMBER)).thenReturn(Optional.of(account));
-        when(accountService.getAccount(EXISTING_ACCOUNT_NUMBER)).thenReturn(account);
+        when(accountRepository.findByAccountNumber(EXISTING_ACCOUNT_NUMBER)).thenReturn(Optional.of(account));
+//        when(accountService.getAccount(EXISTING_ACCOUNT_NUMBER)).thenReturn(account);
 
         //Act & Assert
         assertThatCode(() -> accountInformationService.getBalanceOfAccount(EXISTING_ACCOUNT_NUMBER)).doesNotThrowAnyException();
